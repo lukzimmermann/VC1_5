82915513cdab695d9c4a2920ab03e1d9adb8b302
Lukas Zimmermann <lukas@air-von-lukas.zhaw.ch>
2023-10-24 17:45:35 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import './App.css';
 
 function App() {
   const time = new Date();
-  //time.setHours(12);
-  //time.setMonth(9);
+  time.setHours(12);
+  time.setMonth(9);
 
   const rawSunPosition = SunPosition(15, time, -0.4);
   const sunPosition = [rawSunPosition[0], rawSunPosition[2], rawSunPosition[1]];
   let sunIntensity = rawSunPosition[2];
   if (sunIntensity < 0) sunIntensity = 0;
 
-  const Scene = () => {
+  const Apartment = () => {
     const fbx = useFBX('WohnungVC.fbx');
 
     if (fbx) {
@@ -50,13 +50,14 @@ function App() {
         gl.shadowMap.type = THREE.PCFSoftShadowMap; // Use soft shadows
       }}
     >
-      <Scene receiveShadow castShadow />
+      <PerspectiveCamera makeDefault position={[0, 10, 15]} />
+      <OrbitControls />
+      <Apartment receiveShadow castShadow />
       <mesh position={sunPosition}>
         <sphereGeometry args={[0.3, 100, 100, 1]} />
         <meshStandardMaterial color="#ffff00" />
       </mesh>
 
-      <PerspectiveCamera makeDefault position={[0, 10, 15]} />
       <spotLight
         position={sunPosition}
         intensity={sunIntensity * 50}
@@ -65,7 +66,6 @@ function App() {
         penumbra={1}
       />
       <ambientLight intensity={sunIntensity * 0.2 + 0.3} />
-      <OrbitControls />
     </Canvas>
   );
 }
